45ffbe90b552d1a583fe8e026a20c111198855e1
Chinchane <SUMIT.CHINCHANE@int.asurion.com>
2016-12-26 14:44:44 +0530

    Saple Node js applications

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var morgan = require('morgan');
+var dishrouter = require('./routes/dishes');
+var leadershipRouter  = require('./routes/leadership')
+var promotions = require('./routes/promotions');
+var hostname = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var port = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+var app = express();
+app.use(morgan('dev'));
+app.use('/dishes',dishrouter);
+app.use('/leadership',leadershipRouter);
+app.use('/promotions',promotions);
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port,hostname,function(){
+	console.log('Server Listening on host ' + hostname + ':' + port);
+});
