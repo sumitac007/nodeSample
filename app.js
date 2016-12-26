951cadb9bce9610d12b98bb3aba6545f8e35aec3
Chinchane <SUMIT.CHINCHANE@int.asurion.com>
2016-12-26 14:59:38 +0530

    corrected hostanme and  port

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var morgan = require('morgan');
 var dishrouter = require('./routes/dishes');
 var leadershipRouter  = require('./routes/leadership')
 var promotions = require('./routes/promotions');
-var hostname = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-var port = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var hostname = process.env.OPENSHIFT_NODEJS_IP || 3000;
+var port = process.env.OPENSHIFT_NODEJS_PORT || '127.0.0.1';
 
 var app = express();
 app.use(morgan('dev'));
